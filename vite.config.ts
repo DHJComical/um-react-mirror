6a2dc1acf2a7871dd2d73a3b08da20dfd450f052
Comical <dhjcomical@163.com>
2025-12-14 15:41:58 +0800

    Set base path to /um-react-mirror/ in Vite config
    Configured the Vite project to use '/um-react-mirror/' as the base path. This is necessary for correct asset and routing resolution when deploying to a subdirectory.

--- vite.config.ts
+++ vite.config.ts
@@ -23,6 +23,7 @@ const version = `${pkg.version} (${shortCommit})`;
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/um-react-mirror/',
   worker: {
     format: 'es',
   },
